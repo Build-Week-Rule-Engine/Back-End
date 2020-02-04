177ffcd568cc459d17e15327f6ed09811fafb9d8
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-02-03 19:56:41 -0500

    add dependencies for routes

---
+++ routes/__needs.js
@@ -0,0 +1,13 @@
+/**************************************/
+
+const { Router } = require ('../common')
+
+module.exports = {
+
+  Router,
+
+  config : require ('../config'),
+  middleware : require ('../middleware'),
+  tools : require ('../tools'),
+
+}
