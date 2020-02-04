ad6e72bfe9e956f138adaa7690684d1660db5642
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-02-03 19:59:44 -0500

    add basic trees router

---
+++ routes/trees/router.js
@@ -0,0 +1,29 @@
+/***********************************************************
+  ~/trees - router
+***********************************************************/
+
+const BASE = '/trees'
+
+const {
+  Router,
+  middleware : { amigx, nope },
+} = require ('./__needs')
+
+/**************************************/
+
+const router = Router ()
+
+router.route (BASE)
+.get ((ri, ro) => {
+
+  ro
+  .status (200)
+  .json ({
+    'message' : 'hello world',
+  })
+
+})
+
+/**************************************/
+
+module.exports = router
