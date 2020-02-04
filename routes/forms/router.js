8be5a23341d012a8ca2728d0eb3d2cc3a3085c91
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-02-03 19:59:32 -0500

    add basic forms router

---
+++ routes/forms/router.js
@@ -0,0 +1,29 @@
+/***********************************************************
+  ~/forms - router
+***********************************************************/
+
+const BASE = '/forms'
+
+const {
+  Router,
+  middleware : { amigx, nope },
+} = require ('./__needs')
+
+/**************************************/
+
+const router = Router ()
+
+router.route (BASE)
+.get ((ri, ro) => {
+
+  ro
+  .status (200)
+  .json ({
+    'message' : 'hello world',
+  })
+
+})
+
+/**************************************/
+
+module.exports = router
