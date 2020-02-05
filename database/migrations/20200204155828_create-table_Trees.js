d1f6f62a1ee1204f52f70ced042845ff8ca9ac18
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-02-05 14:06:56 -0500

    setup foreign key referencing Form

--- database/migrations/20200204155828_create-table_Trees.js
+++ database/migrations/20200204155828_create-table_Trees.js
@@ -13,6 +13,13 @@ function up (knex) {
     .increments ('_id')
 
     table
+    .integer ('form_id').unsigned ()
+    .references ('_id').inTable ('Forms')
+    .onUpdate ('CASCADE') // keep Tree, update referenced _id
+    .onDelete ('SET NULL') // keep Tree, but it no longer references a Form
+    .nullable () // in case the referenced Form is ever deleted
+
+    table
     .string ('name', 255)
     .notNullable ()
 
