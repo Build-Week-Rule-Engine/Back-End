c529aef229a555daece0a98352f2f256cc3402e6
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-02-05 15:44:15 -0500

    add dummy seeds for Forms

---
+++ database/seeds/002_Forms.js
@@ -0,0 +1,31 @@
+/**************************************/
+
+module.exports = { seed }
+
+/**************************************/
+
+const TABLE = 'Forms'
+
+function seed (knex) {
+  return (
+    knex (TABLE)
+    .delete () // deletes all entries
+    .then (function () {
+      // Inserts seed entries
+      return knex (TABLE).insert ([
+        {
+          'name' : `tables-example-1`,
+          'data' : `{"name":{"type":"string","value":"name-1"},"date":{"type":"string","value":"date-1"},"company":{"type":"string","value":"company-1"}}`,
+        },
+        {
+          'name' : `tables-example-2`,
+          'data' : `{"name":{"type":"string","value":"name-2"},"date":{"type":"string","value":"date-2"},"company":{"type":"string","value":"company-2"}}`,
+        },
+        {
+          'name' : `tables-example-3`,
+          'data' : `{"name":{"type":"string","value":"name-3"},"date":{"type":"string","value":"date-3"},"company":{"type":"string","value":"company-3"}}`,
+        },
+      ])
+    })
+  )
+}
