88d000c89216ceb7331b4aecb1cee1a5b92ba0c9
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-02-06 15:47:23 -0500

    change order of params

--- middleware/ok/route-info.js
+++ middleware/ok/route-info.js
@@ -7,8 +7,8 @@ module.exports = routeInfo
 const okResponse = require ('./response')
 
 function routeInfo (
-  message = `Hello. Please refer to [info] for available routes and methods.`,
   info,
+  message = `Hello. Please refer to [info] for available routes and methods.`,
 ) {
   return okResponse (200, message, { info })
 }
