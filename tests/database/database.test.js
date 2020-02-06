ff2229cc4e8c4ce50d62f2adbf03384afd599c9a
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-02-06 18:23:20 -0500

    add _very basic_ tests

---
+++ tests/database/database.test.js
@@ -0,0 +1,33 @@
+/**************************************/
+
+const { config, database } = require ('./__needs')
+
+/**************************************/
+
+describe ('database', () => {
+
+  /// STUFF HAPPENS AT ALL ///
+  test (`runs tests`, () => {
+
+    expect (true).toBe (true)
+
+  })
+
+  /// CHECK NODE ENVIRONMENT ///
+  describe (`node environment`, () => {
+
+    test (`needs to be testing`, () => {
+
+      expect (config.NODE_ENV).toEqual ('testing')
+
+    })
+
+  })
+
+  /// ONLY DO THIS IF IN TESTING MODE ///
+
+  if (config.NODE_ENV === 'testing') {
+
+  }
+
+})
---
+++ tests/routes/api-forms.test.js
@@ -0,0 +1,35 @@
+/**************************************/
+
+const request = require ('supertest')
+
+const { config, app } = require ('./__needs')
+
+/**************************************/
+
+describe ('/api/forms', () => {
+
+  /// STUFF HAPPENS AT ALL ///
+  test (`runs tests`, () => {
+
+    expect (true).toBe (true)
+
+  })
+
+  /// CHECK NODE ENVIRONMENT ///
+  describe (`node environment`, () => {
+
+    test (`needs to be testing`, () => {
+
+      expect (config.NODE_ENV).toEqual ('testing')
+
+    })
+
+  })
+
+  /// ONLY DO THIS IF IN TESTING MODE ///
+
+  if (config.NODE_ENV === 'testing') {
+
+  }
+
+})
