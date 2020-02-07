537b2649004bbf0287727faca29ad4a8c278c9b0
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-02-06 19:52:33 -0500

    add tests for GET

--- tests/routes/api-forms.test.js
+++ tests/routes/api-forms.test.js
@@ -2,7 +2,7 @@
 
 const request = require ('supertest')
 
-const { config, app } = require ('./__needs')
+const { config, app, database : { db }, TEST_DATA } = require ('./__needs')
 
 const BASE = '/api/forms'
 
@@ -31,6 +31,61 @@ describe (BASE, () => {
   /// ONLY DO THIS IF IN TESTING MODE ///
 
   if (config.NODE_ENV === 'testing') {
+
+    /// ROUTE INFO ///
+
+    describe (`GET ${BASE}`, () => {
+
+      const PATH = BASE
+      let token
+
+      beforeAll (async () => {
+        await db ('Users').truncate ()
+
+        await request (app)
+        .post ('/auth/sign-up')
+        .send (TEST_DATA.SignUp.good)
+        .then ((re) => {
+          console.log (re.body)
+          token = re.body.token
+        })
+      })
+
+      afterAll (async () => {
+        await db ('Users').truncate ()
+      })
+
+      /// STATUS CODE? ///
+
+      test (`responds with 200 OK`, async () => {
+
+        return (
+          await request (app)
+          .get (PATH)
+          .set ('Authorization', token)
+          .then ((re) => {
+            expect (re.status).toEqual (200)
+          })
+        )
+
+      })
+
+      /// RESPONSE TYPE? ///
+
+      test (`responds with JSON body`, async () => {
+
+        return (
+          await request (app)
+          .get (PATH)
+          .set ('Authorization', token)
+          .then ((re) => {
+            expect (re.type).toMatch (/json/i)
+          })
+        )
+
+      })
+
+    })
 
   }
 
